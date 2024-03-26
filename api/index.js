37a06cf7481e902fc290ba75a6866798a00a7c28
kilvencodes <kilven.coder01@gmail.com>
2024-03-27 07:51:27 +0800

    replace wildcard origin with env var

--- api/index.js
+++ api/index.js
@@ -21,7 +21,12 @@ app.use(express.json());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cookieParser());
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 
 app.get('/', (req, res) => res.send('Express on Vercel'));
 app.use('/api/user', userRoute);
