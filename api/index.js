1e26fff7e4bdfe7e93dd2382d208b2b59963c8a2
kilvencodes <kilven.coder01@gmail.com>
2024-03-27 09:08:32 +0800

    remove header credentials

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
